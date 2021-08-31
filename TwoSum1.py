bdd7361019c92e5aac17b2b092cea69dd59e899e
ljingr <35159049+ljingr@users.noreply.github.com>
2021-08-30 23:50:39 -0400

    Add files via upload

---
+++ TwoSum1.py
@@ -0,0 +1,15 @@
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n in h:
+                return [h[n],i]
+            h[num] = i
